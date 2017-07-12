20e5361377bfbf940120b84a1123a8e435aa3b19
chenlongzhen <longzhen.chen@foxmail.com>
2017-07-12 18:29:09 +0800

    init

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -74,9 +74,9 @@ object processdata extends App {
 
     logger.info("processing data")
 
-    val splitdata: Array[RDD[LabeledPoint]] = process_data(sc, train_path_in, 0.8, 500, "1")
+    val splitdata = process_data(sc, train_path_in, 0.8, 500, "1")
 
-    val train: RDD[LabeledPoint] = splitdata(0)
+    val train = splitdata(0)
     val test = splitdata(1)
     train.saveAsTextFile("/team/ad_wajue/chenlongzhen/train_moba.train")
     test.saveAsTextFile("/team/ad_wajue/chenlongzhen/train_moba.test")
